54d4e31a0d2fa352166f99427b7d5588640d70a1
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-11-26 16:08:10 -0500

    Pseudo for ImportExportFile class

---
+++ src/main/java/baseline/ImportExportFile.java
@@ -0,0 +1,67 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
+ *  Copyright 2021 Michael Andrich
+ */
+
+
+/*
+    This will deal with handling files including importing and exporting files
+    I don't want to lose my mind with html and json files, so it won't be very in depth
+ */
+
+
+
+package baseline;
+
+import baseline.Item;
+
+import java.io.File;
+import java.util.List;
+
+public class ImportExportFile {
+
+    private List<Item> list;
+    private File path;
+
+    // function for importing a text file
+    public List<Item> importFile() {
+        try{
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // will import a TSV text file that reads each line but skips the first line
+    public List<Item> importText() {
+
+    }
+
+    // like the above importFile function, this will export based on the text file
+    public String exportFile() {
+        try{
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // will export as a TSV text file with each attribute being separated by a tab
+    public String exportText() {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
